13a2f761e5ee2e63270fb9cf302612740105ddc4
boris <boris@DESKTOP-BNL1UD6>
2025-03-26 21:45:03 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace prKol_ind1_Borisov
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxInfix = new System.Windows.Forms.TextBox();
+            this.textBoxprefix = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxInfix
+            // 
+            this.textBoxInfix.Location = new System.Drawing.Point(109, 116);
+            this.textBoxInfix.Name = "textBoxInfix";
+            this.textBoxInfix.Size = new System.Drawing.Size(100, 23);
+            this.textBoxInfix.TabIndex = 0;
+            // 
+            // textBoxprefix
+            // 
+            this.textBoxprefix.Location = new System.Drawing.Point(304, 116);
+            this.textBoxprefix.Name = "textBoxprefix";
+            this.textBoxprefix.ReadOnly = true;
+            this.textBoxprefix.Size = new System.Drawing.Size(100, 23);
+            this.textBoxprefix.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(109, 177);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 48);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Преобразовать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(109, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Инфиксное выражение:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(304, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Префиксное выражение:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxprefix);
+            this.Controls.Add(this.textBoxInfix);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxInfix;
+        private System.Windows.Forms.TextBox textBoxprefix;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prKol_ind1_Borisov
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string infix = textBoxInfix.Text;
+            if (string.IsNullOrWhiteSpace(infix))
+            {
+                MessageBox.Show("Введите выражение");
+            }
+            try
+            {
+                string delSpase = String.Join("", infix.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+                char[] sim = delSpase.ToCharArray();
+                Array.Reverse(sim);
+                string reverseInfix = new string(sim).Replace('(', '\0').Replace(')', '(').Replace('\0', ')');
+                Stack<char> stack = new Stack<char>();
+                List<char> output = new List<char>();
+                foreach (char c in reverseInfix)
+                {
+                    if (char.IsLetterOrDigit(c))
+                    {
+                        output.Add(c);
+                    }
+                    else if (c == '(')
+                    {
+                        stack.Push(c);
+                    }
+                    else if (c == ')')
+                    {
+                        while (stack.Peek() != '(' && stack.Count > 0)
+                        {
+                            output.Add(stack.Pop());
+                        }
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        int op = 0;
+                        int stackOp = 0;
+                        if (c == '+' || c == '-')
+                        {
+                            op = 1;
+                        }
+                        else if (c == '*' || c == '/')
+                        {
+                            op = 2;
+                        }
+                        else if (c == '^')
+                        {
+                            op = 3;
+                        }
+                        while (stack.Count > 0)
+                        {
+                            char newStackOp = stack.Peek();
+                            if (newStackOp == '+' || newStackOp == '-')
+                            {
+                                stackOp = 1;
+                            }
+                            else if (newStackOp == '*' || newStackOp == '/')
+                            {
+                                stackOp = 2;
+                            }
+                            else if (newStackOp == '^')
+                            {
+                                stackOp = 3;
+                            }
+                            else break;
+                            if (op <= stackOp)
+                            {
+                            output.Add(stack.Pop());
+
+                            }
+                        }
+                        stack.Push(c);
+                    }
+                }
+                while (stack.Count > 0)
+                {
+                    output.Add(stack.Pop());
+                }
+                char[] outArr = output.ToArray();
+                Array.Reverse(outArr);
+                string prefix = string.Join(" ", outArr);
+                textBoxprefix.Text = prefix;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ошибка преобразования");
+            }
+
+        }
+    }
+}
